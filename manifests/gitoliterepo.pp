52d53f86cf773db2af9979fd25a735c555c06d78
Tom Barber <tom@analytical-labs.com>
2012-06-08 15:47:04 +0100

    first try at repo management

---
+++ manifests/gitoliterepo.pp
@@ -0,0 +1,14 @@
+define git::gitoliterepo($ensure, $name){
+	
+	exec { "export gitolite admin":
+    	command => "git clone git@localhost:gitolite-admin.git /home/git/gitolite-admin",
+    	creates => "/home/git/gitolite-admin",
+	}
+
+	
+	common::concatfilepart {"repo-${name}":
+    	ensure  => $ensure,
+    	file    => /home/git/gitolite-admin/conf/gitolite.conf",
+    	content => template("git/default-repo.erb"),
+  }
+}
