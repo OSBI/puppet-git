1d5771dc8ade4c35c27288a3711305acfe28f8d4
Tom Barber <tom@analytical-labs.com>
2012-06-08 14:24:44 +0100

    changes for gitolite

---
+++ manifests/gitolite.pp
@@ -0,0 +1,50 @@
+class git::gitolite{
+	
+	ssh::sshclient { "git": }
+	
+	package {
+		"gitolite" :
+			ensure => present,
+			require => Ssh::Sshclient["git"],
+	}
+	package {
+		"git" :
+			ensure => present,
+	}
+	package {
+		"git-daemon-run" :
+			ensure => present,
+	}
+	
+  	
+	exec {
+		"create-gitolite" :
+			cwd => "/home/git",
+			command => "git config --global user.email \"git@meteoritehosting.com\" && git config --global user.name \"Meteorite Hosting\" &&/usr/bin/gl-setup /home/git/.ssh/id_rsa.pub",
+			creates => "/home/git/repositories",
+			user => "git",
+			environment => "HOME=/home/git",
+			require => Package["gitolite"],
+	}
+	
+	
+	file {
+		"/home/git/.gitolite/hooks/common/post-receive" :
+			mode => 755,
+			owner => git,
+			group => git,
+			source => "puppet:///modules/cloudbi/post-receive",
+			require => [Package["gitolite"], Exec["create-gitolite"]]
+	}
+	exec {
+		"update-gitolite" :
+			cwd => "/home/git",
+			command => "/usr/bin/gl-setup /home/git/.ssh/id_rsa.pub",
+			creates => "/home/git/repositories/etl.git/hooks/post-receive",
+			user => "git",
+			environment => "HOME=/home/git",
+			require => File["/home/git/.gitolite/hooks/common/post-receive"],
+	}
+	
+	
+}
