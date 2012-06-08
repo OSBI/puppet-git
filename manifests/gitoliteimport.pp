108a5a413fdc950c944a5f934591290e8730d48f
Tom Barber <tom@analytical-labs.com>
2012-06-08 17:26:43 +0100

    import code

---
+++ manifests/gitoliteimport.pp
@@ -0,0 +1,32 @@
+define git::gitoliteimport($repo, $directory=false, $subdir=""){
+	
+	exec { "export ${repo} repo for ${name}":
+		cwd => "/home/git",
+		user => "git",
+		environment => "HOME=/home/git",
+    	command => "git clone git@localhost:${repo}.git /home/git/${repo}",
+    	creates => "/home/git/${repo}",
+	}
+	if ($directory) {
+			exec { "copy directory ${name}":
+			cwd => "/home/git",
+			user => "git",
+			environment => "HOME=/home/git",
+    		command => "cp -rf ${name} /home/git/${repo}/${subdir}",
+    		creates => "/home/git/${repo}/${subdir}/${name}",
+    		require => Exec["export ${name} repo"],
+    	}
+	
+	} else {
+		exec { "copy file ${name}":
+			cwd => "/home/git",
+			user => "git",
+			environment => "HOME=/home/git",
+    		command => "cp -rf ${name} /home/git/${repo}/${subdir}",
+    		creates => "/home/git/${repo}/${subdir}/${name}",
+    		require => Exec["export ${name} repo"],
+	}
+	}
+	
+	
+}
