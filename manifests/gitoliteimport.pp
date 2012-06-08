449824f105f33f8537129ebd0911744ce3094a1e
Tom Barber <tom@analytical-labs.com>
2012-06-08 17:46:18 +0100

    git

--- manifests/gitoliteimport.pp
+++ manifests/gitoliteimport.pp
@@ -10,19 +10,30 @@ define git::gitoliteimport($repo, $directory=false, $subdir=""){
 	if ($directory) {
 			exec { "copy directory ${name}":
 			cwd => "/home/git",
-			user => "git",
-			environment => "HOME=/home/git",
     		command => "cp -rf ${name} /home/git/${repo}/${subdir}",
     		creates => "/home/git/${repo}/${subdir}/${name}",
     		require => Exec["export ${repo} repo for ${name}"],
     	}
-	
+	  exec { "commit ${repo} for ${name}":
+		cwd => "/home/git/gitolite-admin",
+		user => "git",
+		environment => "HOME=/home/git",
+    	command => "git commit -a -m \"import ${name}\" && git push && mkdir -p /home/git/committed/ && touch /home/git/committed/${name}",
+    	creates => "/home/git/committed/${name}"
+	}
 	} else {
 		exec { "copy file ${name}":
 			cwd => "/home/git",
     		command => "cp -rf ${name} /home/git/${repo}/${subdir}",
     		creates => "/home/git/${repo}/${subdir}/${name}",
     		require => Exec["export ${repo} repo for ${name}"],
+	}
+	exec { "commit ${repo} for ${name}":
+		cwd => "/home/git/gitolite-admin",
+		user => "git",
+		environment => "HOME=/home/git",
+    	command => "git commit -a -m \"import ${name}\" && git push && mkdir -p /home/git/committed/ && touch /home/git/committed/${name}",
+    	creates => "/home/git/committed/${name}"
 	}
 	}
 	
