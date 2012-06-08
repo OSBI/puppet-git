1199289b31e6baa318111d3ca078e963d0eaeb00
Tom Barber <tom@analytical-labs.com>
2012-06-08 15:49:33 +0100

    test repo

--- manifests/gitoliterepo.pp
+++ manifests/gitoliterepo.pp
@@ -3,12 +3,10 @@ define git::gitoliterepo($ensure, $name){
 	exec { "export gitolite admin":
     	command => "git clone git@localhost:gitolite-admin.git /home/git/gitolite-admin",
     	creates => "/home/git/gitolite-admin",
-	}
-
-	
+	} ->
 	common::concatfilepart {"repo-${name}":
     	ensure  => $ensure,
-    	file    => /home/git/gitolite-admin/conf/gitolite.conf",
+    	file    => "/home/git/gitolite-admin/conf/gitolite.conf",
     	content => template("git/default-repo.erb"),
   }
 }
