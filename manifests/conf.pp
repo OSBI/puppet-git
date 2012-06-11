e84c374d25580b0f7bb089831b0df9bcc84a4f7c
Tom Barber <tom@analytical-labs.com>
2012-06-11 14:50:17 +0100

    try abstracting

---
+++ manifests/conf.pp
@@ -0,0 +1,9 @@
+define git::conf($ensure=present) {
+
+	common::concatfilepart {"repo-${name}":
+    	ensure  => $ensure,
+    	file    => "/home/git/gitolite-admin/conf/gitolite.conf",
+    	content => template("git/default-repo.erb"),
+  	}
+  	
+}
--- manifests/gitoliterepo.pp
+++ manifests/gitoliterepo.pp
@@ -7,11 +7,10 @@ define git::gitoliterepo($ensure, $key){
     	command => "git clone git@localhost:gitolite-admin.git /home/git/gitolite-admin",
     	creates => "/home/git/gitolite-admin",
 	}
-	common::concatfilepart {"repo-${name}":
-    	ensure  => $ensure,
-    	file    => "/home/git/gitolite-admin/conf/gitolite.conf",
-    	content => template("git/default-repo.erb"),
-  	}
+
+ 	git::conf{ $name:
+		ensure => present,
+	}
   	exec { "commit gitolite admin for ${name}":
 		cwd => "/home/git/gitolite-admin",
 		user => "git",
