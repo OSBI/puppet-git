acfdd65218257bee16e11bd6cb5420816f8f3bd8
Tom Barber <tom@analytical-labs.com>
2012-06-11 14:17:56 +0100

    git reordering

--- manifests/gitoliterepo.pp
+++ manifests/gitoliterepo.pp
@@ -6,7 +6,12 @@ define git::gitoliterepo($ensure, $key){
 		environment => "HOME=/home/git",
     	command => "git clone git@localhost:gitolite-admin.git /home/git/gitolite-admin",
     	creates => "/home/git/gitolite-admin",
-	} ->
+	}
+	common::concatfilepart {"repo-${name}":
+    	ensure  => $ensure,
+    	file    => "/home/git/gitolite-admin/conf/gitolite.conf",
+    	content => template("git/default-repo.erb"),
+  	}
   	exec { "commit gitolite admin for ${name}":
 		cwd => "/home/git/gitolite-admin",
 		user => "git",
