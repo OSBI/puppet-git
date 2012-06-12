66cbfc9e304a854eac83dd9a9621c62f87372cdc
Tom Barber <tom@analytical-labs.com>
2012-06-12 10:50:24 +0100

    file

--- manifests/gitoliterepo.pp
+++ manifests/gitoliterepo.pp
@@ -1,21 +1,21 @@
 define git::gitoliterepo($ensure, $key){
 	
-	#exec { "export gitolite admin for ${name}":
-	#	cwd => "/home/git",
-	#	user => "git",
-	#	environment => "HOME=/home/git",
-    #	command => "git clone git@localhost:gitolite-admin.git /home/git/gitolite-admin",
-    #	creates => "/home/git/gitolite-admin",
-	#}
+	exec { "export gitolite admin for ${name}":
+		cwd => "/home/git",
+		user => "git",
+		environment => "HOME=/home/git",
+    	command => "git clone git@localhost:gitolite-admin.git /home/git/gitolite-admin",
+    	creates => "/home/git/gitolite-admin",
+	}
 
  	git::conf{ $name:
 		ensure => present,
 	}
-  	#exec { "commit gitolite admin for ${name}":
-	#	cwd => "/home/git/gitolite-admin",
-	#	user => "git",
-	#	environment => "HOME=/home/git",
-    #	command => "git commit conf/gitolite.conf -m \"new repo ${name}\" && git push ; mkdir -p /home/git/committed/ ; touch /home/git/committed/${name}",
-    #	creates => "/home/git/committed/${name}"
-	#}
+  	exec { "commit gitolite admin for ${name}":
+		cwd => "/home/git/gitolite-admin",
+		user => "git",
+		environment => "HOME=/home/git",
+    	command => "git commit conf/gitolite.conf -m \"new repo ${name}\" && git push ; mkdir -p /home/git/committed/ ; touch /home/git/committed/${name}",
+    	creates => "/home/git/committed/${name}"
+	}
 }
