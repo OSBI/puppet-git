433f011d686ed04d1e99d8dbf6119216f301c883
Tom Barber <tom@analytical-labs.com>
2012-06-12 14:42:48 +0100

    hook code

---
+++ manifests/hook.pp
@@ -0,0 +1,21 @@
+define git::hook($ensure = "present" ) {
+	
+	file {
+		"/home/git/.gitolite/hooks/common/$name" :
+			mode => 755,
+			owner => git,
+			group => git,
+			source => "puppet:///modules/git/${name}",
+			require => [Package["gitolite"], Exec["create-gitolite"]]
+	} ->
+	exec {
+		"update-gitolite" :
+			cwd => "/home/git",
+			command => "/usr/bin/gl-setup /home/git/.ssh/id_rsa.pub",
+			#creates => "/home/git/repositories/etl.git/hooks/post-receive",
+			unless => "grep \"#FILE CHECK DO NOT REMOVE\" /home/git/repositories/gitolite-admin.git/hooks/post-receive",
+			user => "git",
+			environment => "HOME=/home/git",
+			require => File["/home/git/.gitolite/hooks/common/${name}"],
+	}
+}
