bbef4021bad08e03a13c66ecbf0be966e0661bb7
Tom Barber <tom@analytical-labs.com>
2012-06-12 15:29:03 +0100

    missing hook

--- manifests/hook.pp
+++ manifests/hook.pp
@@ -6,16 +6,14 @@ define git::hook($ensure = "present", $source ='' ) {
 			owner => git,
 			group => git,
 			source => $source,
-			require => [Package["gitolite"], Exec["create-gitolite"]]
 	} ->
 	exec {
-		"update-gitolite" :
+		"update-gitolite $name" :
 			cwd => "/home/git",
 			command => "/usr/bin/gl-setup /home/git/.ssh/id_rsa.pub",
 			#creates => "/home/git/repositories/etl.git/hooks/post-receive",
 			unless => "grep \"#FILE CHECK DO NOT REMOVE\" /home/git/repositories/gitolite-admin.git/hooks/post-receive",
 			user => "git",
 			environment => "HOME=/home/git",
-			require => File["/home/git/.gitolite/hooks/common/${name}"],
 	}
 }
