34c17a0f9088b32e3afc4c05b64384929ba68da8
Tom Barber <tom@analytical-labs.com>
2012-06-12 10:47:05 +0100

    file

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,19 +1,11 @@
 define git::conf($ensure=present) {
 
-#	common::concatfilepart {"repo-${name}":
-#    	ensure  => $ensure,
-#    	file    => "/home/git/gitolite-admin/conf/gitolite.conf",
-#    	content => template("git/default-repo.erb"),
-#  	}
+	common::concatfilepart {"repo-${name}":
+    	ensure  => $ensure,
+    	file    => "/home/git/gitolite-admin/conf/gitolite.conf",
+    	content => template("git/default-repo.erb"),
+    	header => "/tmp/header.txt"
+     	}
 
-common::concatenated_file_part { "${name}" :
-	 dir => "/tmp/files/",
-	 content => template("git/default-repo.erb"),
-	 }
-	 
-#	} ->
-#  	common::concatenated_file { "/tmp/gitolite.conf" :
-#  		dir => "/tmp/files/",
-#  		header => "/tmp/header.txt",
-#  	}
+
 }
