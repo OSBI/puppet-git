9385f29bae7ae85661516b0e468b331305eda67e
Tom Barber <tom@analytical-labs.com>
2012-06-12 13:43:49 +0100

    git

--- manifests/gitoliterepo.pp
+++ manifests/gitoliterepo.pp
@@ -6,16 +6,18 @@ define git::gitoliterepo($ensure, $key){
 		environment => "HOME=/home/git",
     	command => "git clone git@localhost:gitolite-admin.git /home/git/gitolite-admin",
     	creates => "/home/git/gitolite-admin",
-	} ->
+	} 
 
  	git::conf{ $name:
 		ensure => present,
-	} ->
+		require => Exec["export gitolite admin for ${name}"],
+	} 
   	exec { "commit gitolite admin for ${name}":
 		cwd => "/home/git/gitolite-admin",
 		user => "git",
 		environment => "HOME=/home/git",
     	command => "git commit conf/gitolite.conf -m \"new repo ${name}\" && git push ; mkdir -p /home/git/committed/ ; touch /home/git/committed/${name}",
-    	creates => "/home/git/committed/${name}"
+    	creates => "/home/git/committed/${name}",
+    	require => Git::Conf["${name}"],
 	}
 }
