ea0154d99d31a0d001b04d972f189426a8c9ed1a
Tom Barber <tom@analytical-labs.com>
2012-06-12 10:04:52 +0100

    test

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -6,11 +6,11 @@ define git::conf($ensure=present) {
 #    	content => template("git/default-repo.erb"),
 #  	}
 
-common::concatenated_file_part { $name :
+common::concatenated_file_part { "/tmp/files/${name}" :
 	 dir => "/tmp/files/",
 	 content => template("git/default-repo.erb"),
 	} ->
-  	common::concatenated_file { $name :
+  	common::concatenated_file { "/tmp/files/${name}" :
   		dir => "/tmp/files/",
   		header => "/tmp/header.txt",
   	}
