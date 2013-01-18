6c4768b376ac98d88823eaf1594579de3dcdc54f
Tom Barber <tom@analytical-labs.com>
2013-01-18 16:43:28 +0000

    fix import

--- manifests/gitoliteimport.pp
+++ manifests/gitoliteimport.pp
@@ -1,42 +1,38 @@
-define git::gitoliteimport($repo, $directory=false, $subdir="", $file){
-	
-	exec { "export ${repo} repo for ${name}":
-		cwd => "/home/git",
-		user => "git",
-		environment => "HOME=/home/git",
-    	command => "git clone git@localhost:${repo}.git /home/git/${repo}",
-    	creates => "/home/git/${repo}",
-	}
-	if ($directory) {
-			exec { "copy directory ${name}":
-			cwd => "/home/git",
-    		command => "cp -rf ${file} /home/git/${repo}/${subdir}",
-    		creates => "/home/git/${repo}/${subdir}/${file}",
-    		require => Exec["export ${repo} repo for ${name}"],
-    	} ->
-	  exec { "commit ${repo} for ${name}":
-		cwd => "/home/git/${repo}",
-		user => "git",
-		environment => "HOME=/home/git",
-    	command => "git add * ; git commit -a -m \"import ${file}\" ; git push origin master && mkdir -p /home/git/committed/ && touch /home/git/committed/${name}",
-    	creates => "/home/git/committed/${name}"
-	}
-	} else {
-		exec { "copy file ${name}":
-			cwd => "/home/git",
-    		command => "cp -rf ${file} /home/git/${repo}/${subdir}",
-    		creates => "/home/git/${repo}/${subdir}/${name}",
-    		require => Exec["export ${repo} repo for ${name}"],
-	} ->
-	exec { "commit ${repo} for ${name}":
-		cwd => "/home/git/${repo}",
-		user => "git",
-		environment => "HOME=/home/git",
-    	command => "git add * ; git commit -a -m \"import ${name}\" ; git push origin master && mkdir -p /home/git/committed/ && touch /home/git/committed/${name}",
-    	creates => "/home/git/committed/${name}"
-    	
-	}
-	}
-	
-	
+define git::gitoliteimport ($repo, $directory = false, $subdir = "", $file) {
+  exec { "export ${repo} repo for ${name}":
+    cwd         => "/home/git",
+    user        => "git",
+    environment => "HOME=/home/git",
+    command     => "git clone git@localhost:${repo}.git /home/git/${repo}",
+    creates     => "/home/git/${repo}",
+  }
+
+  if ($directory) {
+    exec { "copy directory ${name}":
+      cwd     => "/home/git",
+      command => "cp -rf ${file} /home/git/${repo}/${subdir}",
+      creates => "/home/git/${repo}/${subdir}/${file}",
+      require => Exec["export ${repo} repo for ${name}"],
+    } -> exec { "commit ${repo} for ${name}":
+      cwd         => "/home/git/${repo}",
+      user        => "git",
+      environment => "HOME=/home/git",
+      command     => "git add * ; git commit -a -m \"import ${file}\" ; git push origin master && mkdir -p /home/git/committed/ && touch /home/git/committed/${name}commit",
+      creates     => "/home/git/committed/${name}commit"
+    }
+  } else {
+    exec { "copy file ${name}":
+      cwd     => "/home/git",
+      command => "cp -rf ${file} /home/git/${repo}/${subdir}",
+      creates => "/home/git/${repo}/${subdir}/${name}",
+      require => Exec["export ${repo} repo for ${name}"],
+    } -> exec { "commit ${repo} for ${name}":
+      cwd         => "/home/git/${repo}",
+      user        => "git",
+      environment => "HOME=/home/git",
+      command     => "git add * ; git commit -a -m \"import ${name}\" ; git push origin master && mkdir -p /home/git/committed/ && touch /home/git/committed/${name}commit",
+      creates     => "/home/git/committed/${name}commit"
+    }
+  }
+
 }
